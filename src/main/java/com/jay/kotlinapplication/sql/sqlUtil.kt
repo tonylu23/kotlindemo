e1eb2b9943ac6313725b57897690bfad22059599
rocklu <624826870@qq.com>
2017-06-29 12:01:34 +0800

    ===

--- src/main/java/com/jay/kotlinapplication/sql/sqlUtil.kt
+++ src/main/java/com/jay/kotlinapplication/sql/sqlUtil.kt
@@ -1,8 +1,37 @@
 package com.jay.kotlinapplication.sql
 
+import com.jay.kotlinapplication.App
+
 /**
  * Created by jiayelu on 2017/6/16.
  */
 forecastDbHelper.use {
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance: ForecastDbHelper by lazy { ForecastDbHelper() }
+    }
+}
+
+object CityForestcastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
+
+class ForecastDbHelper(): ManagedSQLiteOpenHelper(App.instance,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
 
 }
